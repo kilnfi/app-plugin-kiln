cc9e73fcadce28464a1439fccef02c35d3bd266c
mxs <mxs@sbrk.org>
2022-06-24 11:59:45 +0200

    fix: clang-analyzer & unused variable warning

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -2,7 +2,6 @@
 
 void handle_provide_token(void *parameters) {
     ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
-    context_t *context = (context_t *) msg->pluginContext;
 
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
